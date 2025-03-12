dc5bdc19048810c5d30f34693a0f1986b7a7ba66
RAMAZANaa <obogs@biosys.local>
2025-03-12 11:54:18 +0300

    Ders2 eklendi

---
+++ Ders-2/ders2-a.php
@@ -0,0 +1,156 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Php Ders-2</title>
+</head>
+<body>
+<!-- 
+Değişken tanımlama;
+1. Değişkenler $ işareti ile tanımlanır.
+2. Mutlaka bir harf veya _ işareti ile başlayabilir.
+3. Değişkenler rakamlar ile başlayamaz.
+4. Değişken içerisinde boşluk ve Türkçe karakterler kullanılamaz.
+5. Değişkenlerde büyük küçük harf duyarlılığı vardır. 
+6. Programlama dili içerisinde bulunan özel tanımlar değişken ismi olarak kullanılamaz.
+-->
+
+<h4>Matematiksel İfadeler</h4>
+<h5>Toplama İşlemi</h5>
+<?php
+    $sayi1=20; //Sayi1 değişkeni tanımlandı
+    $sayi2=17; // Sayi2 değişkeni tanımlandı
+    $sonuc=$sayi1+$sayi2; //Toplama işlemi yapıldı.
+    echo $sonuc; //Sonuç ekrana yazdırıldı.
+    echo "<br>işlemin sonucu: $sayi1+$sayi2=$sonuc"; //sonuç ekrana yazdırıldı.
+    
+    echo "<br><br> Atamalı Toplama İşlemi:";
+
+    $deger=10;
+    $ekdeger=59;
+    echo "<br> Sonuç: $deger + $ekdeger =" . $deger +=$ekdeger; 
+    echo "<br>Değer Değişkeninin İçeriği: $deger";
+    echo "<br>Değer Değişkeninin Yeni İçeriği:". $deger+=123;
+    echo "<br>Değer Değişkeninin Son İçeriği: $deger";
+
+?>
+<h5>Çıkarma İşlemi</h5>
+<?php
+    $sayi1=50; //Sayi1 değişkeni tanımlandı
+    $sayi2=77; // Sayi2 değişkeni tanımlandı
+    $sonuc=$sayi1+$sayi2; //Toplama işlemi yapıldı.
+    echo $sonuc; //Sonuç ekrana yazdırıldı.
+    echo "<br>işlemin sonucu: $sayi1+$sayi2=$sonuc"; //sonuç ekrana yazdırıldı.
+    
+    echo "<br><br> Atamalı Toplama İşlemi:";
+
+    $deger=120;
+    $ekdeger=72;
+    echo "<br> Sonuç: $deger - $ekdeger =" . $deger +=$ekdeger; 
+    echo "<br>Değer Değişkeninin İçeriği: $deger";
+    echo "<br>Değer Değişkeninin Yeni İçeriği:". $deger-=123;
+    echo "<br>Değer Değişkeninin Son İçeriği: $deger";
+
+    ?>
+
+<h5>Çarpma İşlemi</h5>
+<?php
+    $sayi1=50; //Sayi1 değişkeni tanımlandı
+    $sayi2=77; // Sayi2 değişkeni tanımlandı
+    $sonuc=$sayi1*$sayi2; //Toplama işlemi yapıldı.
+    echo $sonuc; //Sonuç ekrana yazdırıldı.
+    echo "<br>işlemin sonucu: $sayi1*$sayi2=$sonuc"; //sonuç ekrana yazdırıldı.
+    
+    echo "<br><br> Atamalı Toplama İşlemi:";
+
+    $deger=120;
+    $ekdeger=72;
+    echo "<br> Sonuç: $deger * $ekdeger =" . $deger *=$ekdeger; 
+    echo "<br>Değer Değişkeninin İçeriği: $deger";
+    echo "<br>Değer Değişkeninin Yeni İçeriği:". $deger*=123;
+    echo "<br>Değer Değişkeninin Son İçeriği: $deger";
+
+    ?>
+
+<h5>Bölme İşlemi</h5>
+<?php
+    $sayi1=50; //Sayi1 değişkeni tanımlandı
+    $sayi2=13; // Sayi2 değişkeni tanımlandı
+    $sonuc=$sayi1/$sayi2; //Toplama işlemi yapıldı.
+    echo $sonuc; //Sonuç ekrana yazdırıldı.
+    echo "<br>işlemin sonucu: $sayi1/$sayi2=$sonuc"; //sonuç ekrana yazdırıldı.
+    
+    echo "<br><br> Atamalı Toplama İşlemi:";
+
+    $deger=120;
+    $ekdeger=72;
+    echo "<br> Sonuç: $deger / $ekdeger =" . $deger /=$ekdeger; 
+    echo "<br>Değer Değişkeninin İçeriği: $deger";
+    echo "<br>Değer Değişkeninin Yeni İçeriği:". $deger/=123;
+    echo "<br>Değer Değişkeninin Son İçeriği: $deger";
+    
+   
+    $sonucyaz=(5*2) + ((55+25)/2) ;
+    echo "<br>$sonucyaz";
+    echo "<br> yenisonuç:".((10*2) + ((55+25)/2));
+    ?>
+
+<h5>Mod İşlemi</h5>
+<?php
+    $sayi1=50; //Sayi1 değişkeni tanımlandı
+    $sayi2=10; // Sayi2 değişkeni tanımlandı
+    $sonuc=$sayi1%$sayi2; //Toplama işlemi yapıldı.
+    echo $sonuc; //Sonuç ekrana yazdırıldı.
+    echo "<br>işlemin sonucu: $sayi1%$sayi2=$sonuc"; //sonuç ekrana yazdırıldı.
+    ?>
+
+<h5>Arttırma İşlemi</h5>
+<?php
+    $sayi1=50; //Sayi1 değişkeni tanımlandı
+   
+    echo "<br>Arttırma İşlemi Yapıldı:". $sayi1++; //Bu satırda hala $sayi değerini yazacaktır fakat bir sonraki 
+    //değişkeni çağırdığımız satırda değişkenin 1 arttırıldığı görüntülenecektir. 
+    //Değişkenin sağında bulunan ++ ifadesi satır yorumlandıktan sonra arttırma işlemini yapar.
+    echo "<br>Değişkein Değeri:". $sayi1; 
+    ?>
+<h5>Arttırma İşlemi</h5>
+<?php
+    $sayi1=50; //Sayi1 değişkeni tanımlandı
+   
+    echo "<br>Arttırma İşlemi Yapıldı:". ++$sayi1; //Değişkenin solunda bulunan ++ ifadesi değişkenin 
+    //değerinin öncelikli olarak arttırılmasını sağlar.
+    echo "<br>Değişkenin Değeri:". $sayi1; 
+
+$sayi2=6;
+echo "<br>Azaltma işlemi yapıldı:".$sayi2--;
+echo "<br>Azaltma işlemi yapıldı:".$sayi2--;
+echo "<br>Azaltma işlemi yapıldı:".$sayi2--;
+
+echo "<br>Değişkenin Değeri:". $sayi2; 
+
+$sayi3=6;
+echo "<br>Arttırma İşlemi Yapıldı:". --$sayi3;
+echo "<br>Arttırma İşlemi Yapıldı:". --$sayi3;
+echo "<br>Arttırma İşlemi Yapıldı:". --$sayi3;
+
+echo "<br>Değişkenin Değeri:". $sayi3; 
+
+$sayi4=6;
+echo "<hr><br>". ++$sayi4."<br>";
+echo "<br>". ++$sayi4."<br>";
+echo "<br>". ++$sayi4."<br>";
+
+$sayi5=6;
+echo "<hr><br>Değişkenin Değeri:".++$sayi5;
+echo "<hr><br>Değişkenin Değeri:".++$sayi5;
+echo "<hr><br>Değişkenin Değeri:".++$sayi5;
+
+
+
+    ?>
+
+
+
+</body>
+</html>
---
+++ Ders-2/ders2-b.php
@@ -0,0 +1,91 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Ders-2</title>
+</head>
+<body>
+<!-- 
+ceil  : Herhangi bir ondalıklı sayıyı yukarı yuvarlamak için
+floor : Herhangi bir ondalıklı sayıyı aşağı yuvarlamak için
+round : Herhangi bir ondalıklı sayıyı en yakına yuvarlamak için
+
+-->    
+<h5>ceil işlemi</h5>
+<?php
+$sayi1 = 14.28563;
+echo "Sonuç=" . ceil($sayi1);
+
+//$sayi1 değişkenin değeri: 14.28563 yukarı yuvarlama işlemi sonucu: ceil ($sayi1) yazdıran kodu yaz
+
+echo "<br> \$Sayi1 değişkeninin değeri $sayi1 yukarı yuvarlama işlemi sonucu:" . ceil ($sayi1); 
+// \ ters eğik çizgi kaçış işaretidir.
+
+
+?>
+
+<h5>floor işlemi</h5>
+<?php
+$sayi1 = 24.98545;
+echo "Sonuç=" . floor($sayi1);
+?>
+
+<h5>round işlemi</h5>
+<?php
+$sayi1 = 13.7541;
+echo "Sonuç=" . round($sayi1);
+
+$sayi2 = 57.323;
+echo "<br>\$sayi2 değişkeninin değeri $sayi2 Round işlemi sonucu:". round($sayi2);
+
+echo "<hr><br> ilk iki hanenin sabit tutularak 3.haneye göre round işleminin uygulanması:";
+$sayi2 = 57.323;
+echo "<br>\$sayi2 değişkeninin değeri $sayi2 Round işlemi sonucu:". round($sayi2,2);
+
+$sayi2 = 57.3298;
+echo "<br>\$sayi2 değişkeninin değeri $sayi2 Round işlemi sonucu:". round($sayi2,3);
+
+
+
+echo "<hr><br> Virgülden öncesi için yuvarlama uygulaması:";
+
+$sayi2 = 54578.323567;
+echo "<br>\$sayi2 değişkeninin değeri $sayi2 Round işlemi sonucu:". round($sayi2,0);
+
+$sayi2 = 54578.323567;
+echo "<br>\$sayi2 değişkeninin değeri $sayi2 Round işlemi sonucu:". round($sayi2,-1);
+
+$sayi2 = 54578.323567;
+echo "<br>\$sayi2 değişkeninin değeri $sayi2 Round işlemi sonucu:". round($sayi2,-2);
+
+$sayi2 = 54578.329847;
+echo "<br>\$sayi2 değişkeninin değeri $sayi2 Round işlemi sonucu:". round($sayi2,-3);
+
+$sayi2 = 54578.323567;
+echo "<br>\$sayi2 değişkeninin değeri $sayi2 Round işlemi sonucu:". round($sayi2,-4);
+
+$sayi2 = 54578.323567;
+echo "<br>\$sayi2 değişkeninin değeri $sayi2 Round işlemi sonucu:". round($sayi2,-5);
+?>
+
+<h5>Number Format işlemi</h5>
+<?php
+$sayi1 = 95450.28563;
+echo "<br> \$Sayi1 değişkeninin değeri $sayi1 yukarı yuvarlama işlemi sonucu:" . number_format ($sayi1); 
+echo "<br> \$Sayi1 değişkeninin değeri $sayi1 yukarı yuvarlama işlemi sonucu:" . number_format ($sayi1,1); 
+echo "<br> \$Sayi1 değişkeninin değeri $sayi1 yukarı yuvarlama işlemi sonucu:" . number_format ($sayi1,2); 
+echo "<br> \$Sayi1 değişkeninin değeri $sayi1 yukarı yuvarlama işlemi sonucu:" . number_format ($sayi1,3); 
+echo "<br> \$Sayi1 değişkeninin değeri $sayi1 yukarı yuvarlama işlemi sonucu:" . number_format ($sayi1,2,",","."). " TL"; 
+echo "<br> \$Sayi1 değişkeninin değeri $sayi1 yukarı yuvarlama işlemi sonucu:" . number_format ($sayi1,0,",","."). " TL"; 
+
+
+
+
+
+?>
+
+
+
+</body>
+</html>
